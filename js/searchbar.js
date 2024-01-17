2513eb40fa249ed9979ef96692a658608a907f73
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-01-17 09:40:27 -0300

    Organização dos comentários e coloquei todas as variaveis em ingles para ficar mais facil de se ler

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,60 +1,41 @@
-// TODO: Da pra colocar o search input e o fetch dentro de funções e carregar eles no onload do body
-// Util pra depois quando eu for fazer os sensores, posso criar uma função que só pega os parametros
-const produtosCardTemplate = document.querySelector("[data-produtos-template]");
-const produtosCardContainer = document.querySelector("[data-produtos-cards-container]");
+// TODO: Separar o que da por funções e deixar o código mais organizado
+const productsCardTemplate = document.querySelector("[data-products-template]");
+const productsCardContainer = document.querySelector("[data-products-cards-container]");
 const searchInput = document.querySelector("[data-search]");
 
-let produtos = [];
+let products = [];
 
-// Essa parte esconde o card caso o input do usuário seja diferente do NOME ou APELIDO no arquivo JSON
-searchInput.addEventListener("input", (e) => {
-    const value = e.target.value.toLowerCase()
-    let algumProdutoCorresponde = false;
-
-    produtos.forEach(produto => {
-        console.log(value)
-        const hasWord = value.split(' ').every(word => produto.nome.toLowerCase().includes(word)) || value.split('.').every(word => produto.apelido.toLowerCase().includes(word))
-        produto.element.classList.toggle("hide", !hasWord)
-
-        if (hasWord) {
-            algumProdutoCorresponde = true;
-        }
-    })
-    let avisoErro = document.getElementById("avisoDeErro");
-    avisoErro.classList.toggle('hide', algumProdutoCorresponde);
-})
-
-// Parte responsável por ler o Json e retornar as informações para a pesquisa e também para o card
+// LEMBRETE: Este fetch é quem lê o arquivos json e cria os cards, atualmente está responvel por
+// criar os modal também. Não sei se isso é o mais certo a se fazer, talvez separar depois.
 fetch("/produtos.json")
     .then(res => res.json())
     .then(data => {
-        // Organiza de forma alfabética pelo nome o json
+        // TODO: Esse data.sort está organizando todos os cards em ordem alfabética a partir do nome
+        // Seria legal ele funcionar só na primeira parte, e quando você pesquisa algo eu deveria desabilitar isso.
         data.sort((a, b) => a.nome.localeCompare(b.nome));
-        produtos = data.map(produto => {
-            console.log("Produto encontrado: " + produto.nome)
+        products = data.map(product => {
+            console.log("Produto encontrado: " + product.nome)
 
             // Cards
-            const card = produtosCardTemplate.content.cloneNode(true).children[0]
-            const nome = card.querySelector("[data-nome]")
+            const card = productsCardTemplate.content.cloneNode(true).children[0]
+            const name = card.querySelector("[data-name]")
             const desc = card.querySelector("[data-desc]")
             const img = card.querySelector("[data-img]")
-            const btnVerMais = card.querySelector("[data-btn-ver-mais]");
-            // const link = card.querySelector("[data-link]")
+            const btn = card.querySelector("[data-btn-ver-mais]");
 
-            nome.textContent = produto.nome
-            desc.textContent = produto.desc
-            img.setAttribute("src", produto.img)
+            name.textContent = product.nome
+            desc.textContent = product.desc
+            img.setAttribute("src", product.img)
+            btn.setAttribute("data-unique-name", product.nome);
 
-            btnVerMais.setAttribute("data-unico-nome", produto.nome);
-
-            // FIXME: Os modals que são aberto não são excluidos depois, preciso arrumar isso.
-            // Essa parte é necessária para que o botão Ver Mais crie um modal para o produto!
-            btnVerMais.addEventListener("click", () => {
+            // Modal
+            btn.addEventListener("click", () => {
                 // Obtenha o nome do produto associado ao botão clicado
-                const nomeUnicoProduto = btnVerMais.getAttribute("data-unico-nome");
-        
+                const modalNameId = btn.getAttribute("data-unique-name");
+                console.log("Nome do Produto no Modal:", modalNameId);
+
                 // Crie um id dinâmico para o modal usando o nome do produto
-                const modalId = `exampleModal-${nomeUnicoProduto}`;
+                const modalId = `exampleModal-${modalNameId}`;
         
                 // Verifique se o modal já foi criado, se não, crie e adicione ao DOM
                 let modalElement = document.getElementById(modalId);
@@ -63,13 +44,13 @@ fetch("/produtos.json")
                     modalElement.id = modalId;
                     modalElement.classList.add('modal', 'fade');
 
-                    // Aqui é colocado a ordem na qual os Guias de Ativação devem aparecer!
+                    // Aqui é colocado a ordem na qual os Guias de Ativação devem aparecer
                     // LEMBRETE: TODOS os produtos devem estar cadastrados aqui.
                     const orderOfFields = ["gl200", "gl300", "gl320mg", "gl500", "gv55", "gv65", "gv300", "gv300n", "gv300can", "gv350mg", "gv355 ceu"];
 
-                    //FIXME: Ao clicar para abrir outro guia ele não fecha o outro
+                    //FIXME: Ao clicar para abrir outro guia ele não fecha o atual
                     const gaModalButton = orderOfFields.map(gaKey => {
-                        const gaBtnSrc = produto.gaModal[gaKey];
+                        const gaBtnSrc = product.gaModal[gaKey];
                         return gaBtnSrc ? `
                             <div class="d-inline-block">
                                 <button class="btn btn-qblue mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${gaKey}" aria-expanded="false" aria-controls="collapse${gaKey}">
@@ -79,7 +60,7 @@ fetch("/produtos.json")
                     }).join('');
 
                     const gaModalContentImg = orderOfFields.map(gaKey => {
-                        const gaImgSrc = produto.gaModal[gaKey];
+                        const gaImgSrc = product.gaModal[gaKey];
                         return gaImgSrc ? `
 
                             <div class="collapse" id="collapse${gaKey}">
@@ -87,15 +68,16 @@ fetch("/produtos.json")
                             </div>` : '';
                     }).join('');
 
+                    // TODO: Retirar o botão Guia de Ativação, trocar por apenas um título
                     modalElement.innerHTML = `
                         <div class="modal-dialog modal-xl">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="${modalId}">${produto.nome} - ${produto.apelido}</h5>
+                                    <h5 class="modal-title" id="${modalId}">${product.nome} - ${product.apelido}</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    <p>${produto.descModal}</p>
+                                    <p>${product.descModal}</p>
                                     <div>
                                         <button class="btn btn-qblue mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapseGuias" aria-expanded="false" aria-controls="collapseGuias">
                                             Guias de Ativação
@@ -114,29 +96,46 @@ fetch("/produtos.json")
                     `;
                     document.body.appendChild(modalElement);
                 }
-        
-                // Remover event listeners antigos antes de adicionar um novo
-                btnVerMais.removeEventListener('click', () => {});
 
-                // Adicionar ouvinte de evento para remover o modal do DOM após ser fechado
-                modalElement.addEventListener('hidden.bs.modal', () => {
-                    // Remover o elemento do DOM
-                    document.body.removeChild(modalElement);
-                });
+                // btn.removeEventListener('click', () => {});
 
                 // Abra o modal correspondente ao produto
                 const modal = new bootstrap.Modal(modalElement);
+                console.log(modal)
                 modal.show();
-        
-                 // Adicionar ouvinte de evento ao modalElement para liberar recursos
-                modalElement.addEventListener('hidden.bs.modal', () => modal.dispose());
 
-                console.log("Nome do Produto no Modal:", nomeUnicoProduto);
+                // Adiciona um event listener para remover o modal do DOM após ser fechado
+                modalElement.addEventListener('hidden.bs.modal', () => {
+                    document.body.removeChild(modalElement);
+                });
+        
+                // Adicionar ouvinte de evento ao modalElement para liberar recursos
+                modalElement.addEventListener('hidden.bs.modal', () => modal.dispose());
             });
     
-            produtosCardContainer.append(card)
+            productsCardContainer.append(card)
 
-            // Aqui eu retorno um objeto para ser usado na busca.
-            return {nome: produto.nome, apelido: produto.apelido, element: card}
+            // Aqui eu retorno um objeto para ser usado na busca. 
+            return {name: product.nome, alias: product.apelido, element: card}
         });
     });
+
+
+// Essa parte esconde o card caso o input do usuário seja diferente do NOME ou APELIDO no arquivo JSON
+searchInput.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase()
+    let matchedProduct = false;
+
+    products.forEach(product => {
+        console.log(value)
+        const hasWord = value.split(' ').every(word => product.name.toLowerCase().includes(word)) || value.split('.').every(word => product.alias.toLowerCase().includes(word))
+        product.element.classList.toggle("hide", !hasWord)
+
+        if (hasWord) {
+            matchedProduct = true;
+        }
+    })
+    let errorWarning = document.getElementById("errorWarning");
+    errorWarning.classList.toggle('hide', matchedProduct);
+})
+
